83777efb2fdc399e2fb3dd4915b1bd58e54c8349
CosmicAmigo <163631997+CosmicAmigo@users.noreply.github.com>
2026-06-04 08:02:31 +0300

    Convert businesses router to ES modules syntax

--- server/routes/businesses.js
+++ server/routes/businesses.js
@@ -1,7 +1,7 @@
+import express from 'express';
+import * as db from '../db.js';
 
-const express = require('express');
 const router = express.Router();
-const db = require('../db');
 
 router.get('/:userId/businesses', async (req, res) => {
     try {
@@ -21,4 +21,4 @@ router.post('/:userId/businesses', async (req, res) => {
     }
 });
 
-module.exports = router;
+export default router;
